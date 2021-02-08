df6818f07f7f6a656fdad75bc5a4f8f1e28d5996
Arut Jinadit <arut.jinadit@gmail.com>
2021-02-08 11:17:00 +0700

    [Modified]: extract implementation into a smaller library.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,14 +1,10 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
-pub struct Config {
-    pub query: String,
-    pub filename: String,
-}
+use minigrep::Config;
 
 fn main() {
+    // --snip--
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -16,28 +12,13 @@ fn main() {
         process::exit(1);
     });
 
+    println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
-}
+    if let Err(e) = minigrep::run(config) {
+        // --snip--
+        println!("Application error: {}", e);
 
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
