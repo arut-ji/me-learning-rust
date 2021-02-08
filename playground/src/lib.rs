6dec358a9b6ba6656825684b1d3f6d60cec2116b
Arut Jinadit <arut.jinadit@gmail.com>
2021-02-08 11:40:36 +0700

    [Modified]: fixed indentations

--- playground/src/lib.rs
+++ playground/src/lib.rs
@@ -10,7 +10,6 @@ pub struct Config {
 
 impl Config {
   pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    // --snip--
     if args.len() < 3 {
       return Err("not enough arguments");
     }
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -4,21 +4,19 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    // --snip--
-    let args: Vec<String> = env::args().collect();
+	let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+	let config = Config::new(&args).unwrap_or_else(|err| {
+		println!("Problem parsing arguments: {}", err);
+		process::exit(1);
+	});
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+	println!("Searching for {}", config.query);
+	println!("In file {}", config.filename);
 
-    if let Err(e) = minigrep::run(config) {
-        // --snip--
-        println!("Application error: {}", e);
+	if let Err(e) = minigrep::run(config) {
+		println!("Application error: {}", e);
 
-        process::exit(1);
-    }
+		process::exit(1);
+	}
 }
