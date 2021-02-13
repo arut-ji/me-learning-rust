ab6d5afa66d7c1753fe6635f43eff19a12f1b880
Arut Jinadit <arut.jinadit@gmail.com>
2021-02-13 19:01:35 +0700

    [Added]: HashMap marcro

--- me-with-marcros/src/main.rs
+++ me-with-marcros/src/main.rs
@@ -1,8 +1,22 @@
+use std::collections::HashMap;
+
+#[allow(unused_macros)]
 macro_rules! hey {
-    () => {}
+    ($name:expr) => {
+        println!("Hey {}", $name);
+    }
 }
 
+macro_rules! map {
+    ($( $key: expr => $value: expr), *) => {{
+        let mut hm = HashMap::new();
+        $(hm.insert($key,$value);)*
+        hm
+    }};
+}
+
+
 fn main() {
-    hey!();
-    println!("Hello, world!");
+    let m = map!("meow" => "hello", "meo" => "hello");
+    println!("{:?}", m);
 }
